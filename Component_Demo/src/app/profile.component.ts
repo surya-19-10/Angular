6174f2ea33e86d39749d918b90131595147aa458
Surya <damasprakash@gmail.com>
2025-01-19 19:25:40 +0530

    made skeleton base code

--- Component_Demo/src/app/profile.component.ts
+++ Component_Demo/src/app/profile.component.ts
@@ -7,11 +7,5 @@ import { Component, ViewEncapsulation } from "@angular/core";
     styleUrl: './profile.component.css'
 })
 export class ProfileComponent{
-    author="Dama";
-    age=23;
-    date = new Date().toLocaleDateString();
-    time:string = new Date().toLocaleTimeString();
-    getAge() {
-        return this.age;
-    }
+    
 }
